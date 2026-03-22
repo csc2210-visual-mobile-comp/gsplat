6ea5c7d75d38263f0606be3e0d7ba47b3d9f6833
viriyadhika <viriya.dhika0@gmail.com>
2026-03-22 18:03:37 -0400

    refnerf dataset

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
-pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu130
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu128
 pip install -r examples/requirements.txt --no-build-isolation
 # git submodule update --init --recursive
 pip install -e . --no-build-isolation
 
 
-cd examples
-python datasets/download_dataset.py
+cd LoRA_modification_and_tests
+python datasets/download_dataset.py --dataset refnerf
