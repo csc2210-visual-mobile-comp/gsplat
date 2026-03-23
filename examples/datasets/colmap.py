11710d4a23ea045472667191f12eea4ecc352968
AlizaM <minkov.al@gmail.com>
2026-03-23 09:43:12 -0400

    fix for lower- upper case colmap names

--- examples/datasets/colmap.py
+++ examples/datasets/colmap.py
@@ -199,6 +199,8 @@ class Parser:
                 colmap_image_dir, image_dir + "_png", factor=factor
             )
             image_files = sorted(_get_rel_paths(image_dir))
+        colmap_name_map = {f.lower(): f for f in colmap_files}
+        image_names = [colmap_name_map.get(f.lower(), f) for f in image_names]
         colmap_to_image = dict(zip(colmap_files, image_files))
         image_paths = [os.path.join(image_dir, colmap_to_image[f]) for f in image_names]
 
